679fe9f0bac1a5ff3a9924b353eaac1b4e5c9f19
shuklanitin24 <shuklanitin107@gmail.com>
2023-01-24 00:11:53 +0530

    Add files via upload

---
+++ 9.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 1;
+    int b = 2;
+
+    if(a--> 0||++b>2){
+        cout<<"Stage1";
+    }
+    else{
+        cout<<"stage2";
+    }
+    cout<< a <<" "<< b << endl;
+}
